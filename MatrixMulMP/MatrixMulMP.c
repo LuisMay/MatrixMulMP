ac043d77ee61b560c987a7303fb997a58798a99b
Luis May <luis-may_@outlook.com>
2022-04-24 01:10:02 -0500

    feat: Matrix loading

---
+++ MatrixMulMP/MatrixMulMP.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+FILE *pFile;		//File handler for matrix files
+
+float* A = NULL;	//Pointer to matrix A
+float* B = NULL;	//Pointer to matrix B
+float* C = NULL;	//Pointer to matrix C
+
+//Variables to save exec time
+time_t start, end;
+
+errno_t err;		//File error catcher
+
+char fName[50]="";	//Name of the file
+int rA, cA, rB, cB, rC, cC = 0;
+
+int main(void) {
+	/*-----------------------------Llenado de matriz A-----------------------------*/
+	printf("Introduzca relative path al archivo que contiene la matriz A: ");
+	scanf_s("%s", &fName, 50);
+	printf("Introduzca las dimenciones de la matriz A (filas columnas): ");
+	scanf_s("%d %d", &rA, &cA);
+	err = fopen_s(&pFile,fName, "r");
+	A = (float*)malloc(sizeof(float)*rA*cA);
+	if (err == 0)
+		for (int i = 0; i < rA; i++)
+			for (int j = 0; j < cA; j++)
+				if (!feof(pFile)) {
+					fscanf_s(pFile, "%d", *(A + i * cA + j));
+				}
+				else{
+					printf("Elementos insuficientes en el archivo, saliendo...\n");
+					return 2;
+				}
+	else {
+		printf("No se pudo abrir el archivo %s, saliendo...\n", fName);
+		return 1;
+	}
+	fclose(pFile);
+
+	/*-----------------------------Llenado de matriz B-----------------------------*/
+	printf("Introduzca relative path al archivo que contiene la matriz B: ");
+	scanf_s("%s", &fName, 50);
+	printf("Introduzca las dimenciones de la matriz B (filas columnas): ");
+	scanf_s("%d %d", &rB, &cB);
+	err = fopen_s(&pFile, fName, "r");
+	B = (float*)malloc(sizeof(float)*rB*cB);
+	if (err == 0)
+		for (int i = 0; i < rB; i++)
+			for (int j = 0; j < cB; j++)
+				if (!feof(pFile)) {
+					fscanf_s(pFile, "%d", *(B + i * cB + j));
+				}
+				else {
+					printf("Elementos insuficientes en el archivo, saliendo...\n");
+					return 2;
+				}
+	else {
+		printf("No se pudo abrir el archivo %s, saliendo...\n", fName);
+		return 1;
+	}
+	fclose(pFile);
+
+	return 0; 
+}

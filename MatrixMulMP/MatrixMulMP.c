b1d98295349feee1aa2c5f1f37d8c04a6bcbcd01
Alessandro Balcazar <alessbalkazar@outlook.com>
2022-05-14 18:06:56 -0500

    44 55 no jala

--- MatrixMulMP/MatrixMulMP.c
+++ MatrixMulMP/MatrixMulMP.c
@@ -5,18 +5,19 @@
 
 FILE *pFile;		//File handler for matrix files
 
-float* A = NULL;	//Pointer to matrix A
-float* B = NULL;	//Pointer to matrix B
-float* C = NULL;	//Pointer to matrix C
+float* matrixA = NULL;	//Pointer to matrix A
+float* matrixB = NULL;	//Pointer to matrix B
+float* matrixC = NULL;	//Pointer to matrix C
 
 //Variables to save exec time
 time_t start, end;
 
 errno_t err;		//File error catcher
 
-char fName[50]="";	//Name of the file
+char fName[100];	//Name of the file
 int rA, cA, rB, cB, rC, cC = 0;
 
+//Print matrix
 void printMatrix(float* matrix, int nrow, int ncol) {
 	for (int i = 0; i < nrow; i++) {
 		for (int j = 0; j < ncol; j++)
@@ -25,55 +26,59 @@ void printMatrix(float* matrix, int nrow, int ncol) {
 	}
 }
 
-int main(void) {
-	/*-----------------------------Llenado de matriz A-----------------------------*/
-	printf("Introduzca las dimenciones de la matriz A (filas columnas): ");
-	scanf_s("%d %d", &rA, &cA);
+// Fill matrix from console
+void fillMatrixUI(char* fName, float* matrix, int rows, int cols){
 	err = fopen_s(&pFile, fName, "r");
-	A = (float*)malloc(sizeof(float)*rA*cA);
 	if (err == 0){
-		for (int i = 0; i < rA; i++)
-			for (int j = 0; j < cA; j++)
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
 				if (!feof(pFile)) {
-					fscanf_s(pFile, "%f", (A + i * cA + j));
+					fscanf_s(pFile, "%f", (matrix + i * cols + j));
 				}
 				else {
 					printf("Elementos insuficientes en el archivo, saliendo...\n");
 					return 2;
 				}
-		printMatrix(A, rA, cA);
+		// Impresion de la matriz
+		printMatrix(matrix, rows, cols);
 	}	
 	else {
-		// Impresion de la matriz
 		printf("No se pudo abrir el archivo %s, saliendo...\n", fName);
 		return 1;
 	}
 	fclose(pFile);
+}
+ 
+/********************/
+/*** Main program ***/
+/********************/
+int main(void){
+	int validMatrix = 0;
+	//do
+	//{
+		/*-----------------------------Llenado de matriz A-----------------------------*/
+		printf("Introduzca las dimensiones de la matriz A (filas columnas): ");
+		scanf_s("%d %d", &rA, &cA);
+		strcpy_s(fName, 100, "D:\\Users\\Alessandro Balcazar\\Documents\\Proyecto_Multi\\MatrixMulMP\\Debug\\matrixA2500.txt");
+		matrixA = (float*)malloc(sizeof(float) * rA * cA);
+		fillMatrixUI(fName, matrixA, rA, cA);
 
-	/*-----------------------------Llenado de matriz B-----------------------------*/
-	printf("Introduzca relative path al archivo que contiene la matriz B: ");
-	scanf_s("%s", &fName, 50);
-	printf("Introduzca las dimenciones de la matriz B (filas columnas): ");
-	scanf_s("%d %d", &rB, &cB);
-	err = fopen_s(&pFile, fName, "r");
-	B = (float*)malloc(sizeof(float)*rB*cB);
-	if (err == 0) {
-		for (int i = 0; i < rB; i++)
-			for (int j = 0; j < cB; j++)
-				if (!feof(pFile)) {
-					fscanf_s(pFile, "%f", (B + i * cB + j));
-				}
-				else {
-					printf("Elementos insuficientes en el archivo, saliendo...\n");
-					return 2;
-				}
-		printMatrix(B, rB, cB);
-	}
-	else {
-		printf("No se pudo abrir el archivo %s, saliendo...\n", fName);
-		return 1;
-	}
-	fclose(pFile);
+		/*-----------------------------Llenado de matriz B-----------------------------*/
+		printf("Introduzca las dimensiones de la matriz B (filas columnas): ");
+		scanf_s("%d %d", &rB, &cB);
+		strcpy_s(fName, 100, "D:\\Users\\Alessandro Balcazar\\Documents\\Proyecto_Multi\\MatrixMulMP\\Debug\\matrixB2500.txt");
+		err = fopen_s(&pFile, fName, "r");
+		matrixB = (float*)malloc(sizeof(float) * rB * cB);
+		fillMatrixUI(fName, matrixA, rB, cB);
+
+		/*-----------Validacion para poder multuplicar las matrices-----------*/
+		if (cA != rB) {
+			printf("Las matrices no se pueden multiplicar. Verifique que las filas y columnas.");
+		}
+		else {
+			validMatrix = 1;
+		}
+	//} while (validMatrix == 0);
 
 	return 0; 
 }
